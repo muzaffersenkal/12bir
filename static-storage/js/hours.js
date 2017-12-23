e5f00ffdd3272f7598b05e169e21dc20bf0ca6df
U140709008 <muzaffer727@hotmail.com>
2017-12-23 17:56:39 +0300

    rezervation selected function

--- static-storage/js/hours.js
+++ static-storage/js/hours.js
@@ -7,12 +7,14 @@ function Hours() {
     this.html = "";
 }
 (function ($, document, window) {
-    Hours.prototype.generateHTML = function() {
+    Hours.prototype.generateHTML = function(secilen) {
         var hours = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];
         var today = new Date();
 
 
         var html = "";
+
+        var className;
 
 
 
@@ -29,9 +31,13 @@ function Hours() {
 
 
 
+                if(secilen == hours[i]){
+                    className = "available a-hours a-hours-selected"
+                }else{
+                        className = "available a-hours"
+                }
 
-
-                html += "<td class='available'><span>" + hours[i] + "</span></td>";
+                html += "<td class='"+ className +"'><span>" + hours[i] + "</span></td>";
 
 
             column++;
