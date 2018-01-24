95c7aafc8f4f0c07c860f378c140af69ada267cf
muzaffersenkal <muzaffer727@hotmail.com>
2018-01-25 00:42:01 +0300

    sepet configuration

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
 from django import forms
 from django.contrib import auth
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, ButtonHolder, Submit,Div
+from field.models import Address
 
 
 
@@ -16,4 +20,57 @@ class UserCreationForm(forms.Form):
         fields = ('username','email','password1','password2')
 
 
+class AddressForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(AddressForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_class = 'form-horizontal'
+        # NEW:
+        self.helper.label_class = ''
+        self.helper.field_class = ''
+        self.helper.textarea_class = ''
+
+
+        self.helper.layout = Layout(
+            Div(
+                Div(Field('title', css_class='input-text full-width'), css_class='col-sm-6 col-md-5'),css_class="form-group row"),
+
+            Div(
+            Div(Field('firstName', rows="3", css_class='input-text full-width'),css_class='col-sm-6 col-md-5'),
+            Div(Field('lastName', rows="3", css_class='input-text full-width'),css_class='col-sm-6 col-md-5'),css_class="form-group row"),
+
+            Div(
+                Div(Field('email', rows="3", css_class='input-text full-width'), css_class='col-sm-6 col-md-5'),
+                Div(Field('phone_number', rows="3", css_class='input-text full-width'), css_class='col-sm-6 col-md-5'),
+                css_class="form-group row"),
+
+            Div(
+                Div(Field('city', rows="3", css_class='input-text full-width'), css_class='col-sm-6 col-md-5'),
+                Div(Field('ilce', rows="3", css_class='input-text full-width'), css_class='col-sm-6 col-md-5'),
+                css_class="form-group row"),
+
+            Div(
+                Div(Field('address', rows="4", placeholder='Adresi giriniz'), css_class='col-sm-12 col-md-12'),
+               css_class="form-group row"),
+
+
+
+            # NEW:
+            # ButtonHolder(
+            #     Submit('submit', 'Submit', css_class='input-text full-width')
+            # )
+        )
+        self.fields['title'].label = "Adres Başlığı"
+        self.fields['firstName'].label = "İsim"
+        self.fields['lastName'].label = "Soyisim"
+        self.fields['email'].label = "Email Adresi"
+        self.fields['phone_number'].label = "Telefon Numarası"
+        self.fields['city'].label = "Şehir"
+        self.fields['ilce'].label = "İlçe"
+        self.fields['address'].label = "Adres"
+
+    class Meta:
+        model = Address
+        fields = ['title', 'firstName','lastName','email','phone_number','city','ilce','address']
 
--- account/views.py
+++ account/views.py
@@ -5,11 +5,12 @@ from django.shortcuts import render
 from django.http import Http404
 from django.views.generic.edit import FormView
 from .forms import UserCreationForm
-from django.views.generic import ListView , TemplateView,DetailView,DeleteView
+from django.views.generic import ListView , TemplateView,DetailView,DeleteView,CreateView
 from .mixins import LoggedInMixin
 from django.contrib.auth.models import User
 from field.models import Sepet,Address
 from django.core.urlresolvers import reverse_lazy
+from .forms import AddressForm
 
 class UserRegister(FormView):
     template_name ="registration/register.html"
@@ -28,11 +29,20 @@ class ProfileView(LoggedInMixin,TemplateView):
 
     template_name = "profile.html"
 
-class SepetView(LoggedInMixin,ListView):
+class SepetView(LoggedInMixin,ListView,FormView):
     template_name = "sepetim.html"
 
-
+    form_class = AddressForm
     context_object_name ="sepet_list"
+    success_url = reverse_lazy('sepetim')
+
+    def form_valid(self, form):
+        print(form)
+        address = form.save(commit=False)
+        address.user = self.request.user
+        address.save()
+        return super(SepetView, self).form_valid(form)
+
 
     def get_queryset(self):
         return Sepet.objects.filter(user=self.request.user)
@@ -41,6 +51,16 @@ class SepetView(LoggedInMixin,ListView):
         context = super(SepetView, self).get_context_data(*args, **kwargs)
         # Add in a QuerySet of all the books
         address = Address.objects.filter(user=self.request.user)
+
+        totalPrice = 0;
+        sepet_list = self.get_queryset()
+        for sepet in sepet_list:
+            totalPrice += sepet.field.price
+
+
+
+
+        context['totalPrice'] = totalPrice
         context['address_list'] = address
 
         return context
@@ -49,12 +69,18 @@ class SepetView(LoggedInMixin,ListView):
 class SepetDeleteView(DeleteView):
     model = Sepet
     success_url = reverse_lazy('sepetim')
+
     def get_object(self, queryset=None):
         """ Hook to ensure object is owned by request.user. """
         obj = super(SepetDeleteView, self).get_object()
         if not obj.user == self.request.user:
             raise Http404
         return obj
+
+class AddressAddView(CreateView):
+    model = Address
+
+
 
 
 def sepetHeader(request):
